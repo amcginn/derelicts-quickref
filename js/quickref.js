e6777d5c9afebeb01bc1b04222df60d5286f5290
Aaron McGinn <aaron.mcginn@oracle.com>
2024-01-18 19:32:03 -0600

    initial elites

--- js/quickref.js
+++ js/quickref.js
@@ -57,6 +57,7 @@ function fill_section(data, parentname, type) {
 }
 
 function init() {
+    fill_section(data_elites, "basic-characters", "Elite");
     fill_section(data_movement, "basic-movement", "Move");
     fill_section(data_action, "basic-actions", "Action");
     fill_section(data_bonusaction, "basic-bonus-actions", "Bonus action");
