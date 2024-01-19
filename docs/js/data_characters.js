74263f354534ab7012f171b24c793b6bdb4cafac
Aaron McGinn <aaron.mcginn@oracle.com>
2024-01-19 04:16:28 -0600

    add characters section with icons and quick taglines

---
+++ docs/js/data_characters.js
@@ -0,0 +1,68 @@
+data_characters = [
+  {
+    title: "Mogli / Corrosis",
+    icon: "minotaur",
+    subtitle: "Jacob",
+    description: "Minotaur warlock - Leader of the Derelicts",
+    reference: "",
+    bullets: [
+      "Minotaur inhabited by an infernal, Corrosis.",
+      "Began forming the Derelicts when he began protecting other exiles and misfits after his own exiling.",
+      "Is fairly well known throughout the world."
+    ]
+  },
+  {
+    title: "Nep Nik / Claudia",
+    icon: "squirrel",
+    subtitle: "David",
+    description: "Squirrel Construct rogue",
+    reference: "",
+    bullets: [
+    ]
+  },
+  {
+    title: "Myra",
+    icon: "barn-owl",
+    subtitle: "Jordan",
+    description: "Dwarvenguard monk (wereowl)",
+    reference: "",
+    bullets: [
+    ]
+  },
+  {
+    title: "Hákarl / Yákovná & Káshi",
+    icon: "shark-jaws",
+    subtitle: "Stephen",
+    description: "Shark demigod divided",
+    reference: "",
+    bullets: [
+    ]
+  },
+  {
+    title: "Gróa",
+    icon: "dragon-orb",
+    subtitle: "Aaron",
+    description: "Dragonborn sorceress",
+    reference: "",
+    bullets: [
+    ]
+  },
+  {
+    title: "Merrek",
+    icon: "bear-head",
+    subtitle: "Jordan",
+    description: "Dwarvenguard ranger (werebear)",
+    reference: "",
+    bullets: [
+    ]
+  },
+  {
+    title: "???",
+    icon: "knapsack",
+    subtitle: "Morgan",
+    description: "Mimic bag",
+    reference: "",
+    bullets: [
+    ]
+  }
+]
